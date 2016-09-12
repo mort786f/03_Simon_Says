aa814380f90536f805f6bd4af025bd0d10ffbfc9
morte <morte@DESKTOP-AJQ2HRF>
2016-09-12 10:31:25 +0200

    Add project files.

---
+++ 03_Simon_Says/Simon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _03_Simon_Says
+{
+    internal class Simon
+    {
+        internal string Echo(string v)
+        {
+            return v.ToLower();
+        }
+
+        internal string Shout(string v)
+        {
+            ();
+        }
+
+        //internal string Repeat(string v)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
